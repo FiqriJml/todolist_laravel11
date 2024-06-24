583108b69affebfeeb8052af394334840e6ff02b
fiqrijml <jamalfiqri@gmail.com>
2024-06-24 19:27:38 +0800

    Centering the logo in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <nav class="bg-white shadow">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between h-16">
-                <div class="flex">
+                <div class="flex items-center">
                     <div class="flex-shrink-0">
                         <a href="{{ url('/') }}" class="text-xl font-bold text-gray-900">Logo</a>
                     </div>
